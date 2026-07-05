21d378cd84e953020c4568faffe75bbccd7dd81c
narchobanyan <narek.chobanyan@moneteam.com>
2026-07-05 11:34:13 +0400

    Updates

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
 self.addEventListener('install', () => self.skipWaiting())
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()))
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => new Response('', { status: 503 }))))
+self.addEventListener('fetch', e => {
+  if (new URL(e.request.url).origin !== self.location.origin) return
+  e.respondWith(fetch(e.request).catch(() => new Response('', { status: 503 })))
+})
